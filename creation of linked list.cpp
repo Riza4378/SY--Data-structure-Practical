304e4c9ccc47d87b32f74d9aa756f4f09e007500
Riza4378 <sajjadshaikh8790@gmail.com>
2026-08-06 13:47:26 +0530

    Add files via upload

---
+++ creation of linked list.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+struct node 
+{
+	int data;
+    struct node* next;
+    
+};
+int main()
+{
+	struct node*newnode=new (struct node);
+	
+	newnode->data=10;
+	newnode->next=NULL;
+	
+	cout<<"data ="<<newnode->data<<endl;
+	cout<<"next ="<<newnode->next<<endl;
+	
+	return 0;
+	
+}
+
